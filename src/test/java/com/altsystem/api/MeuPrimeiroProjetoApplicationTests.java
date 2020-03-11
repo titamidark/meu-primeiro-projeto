d8c911c1c3b3574ee42a1bc76270448357c7cb35
Altamir Caetano de Oliveira <altamir.oliveira@resourceit.com>
2020-03-11 13:43:35 -0300

    Versão inicial do Projeto

---
+++ src/test/java/com/altsystem/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altsystem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
